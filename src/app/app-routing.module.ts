2c89cbec8e8e965f5ecf9b034d39fee642df7c3e
tomerOko <tomeroko2@gmail.com>
2024-11-12 10:46:57 +0200

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,41 @@ const routes: Routes = [
         (m) => m.TasksModule
       ),
   },
+  {
+    path: 'click-send-console',
+    loadChildren: () =>
+      import(
+        './projects/connect_to_backend/click-send-console/click-send-console.module'
+      ).then((m) => m.ClickSendConsoleModule),
+  },
+  {
+    path: 'show-data',
+    loadChildren: () =>
+      import('./projects/connect_to_backend/show-data/show-data.module').then(
+        (m) => m.ShowDataModule
+      ),
+  },
+  {
+    path: 'show-data-with-rxjs',
+    loadChildren: () =>
+      import(
+        './projects/connect_to_backend/show-data-with-rxjs/show-data-with-rxjs.module'
+      ).then((m) => m.ShowDataWithRxjsModule),
+  },
+  {
+    path: 'show-data-according-to-convensions',
+    loadChildren: () =>
+      import(
+        './projects/connect_to_backend/show-data-according-to-convensions/show-data-according-to-convensions.module'
+      ).then((m) => m.ShowDataAccordingToConvensionsModule),
+  },
+  {
+    path: 'show-data-best-practices',
+    loadChildren: () =>
+      import(
+        './projects/connect_to_backend/show-data-best-practices/show-data-best-practices.module'
+      ).then((m) => m.ShowDataBestPracticesModule),
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { provideHttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import {
   BrowserModule,
@@ -13,10 +14,10 @@ import { InputTextModule } from 'primeng/inputtext';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClickSendConsoleComponent } from './projects/connect_to_backend/click-send-console/click-send-console.component';
-import { ShowDataAccordingToConvensionsComponent } from './projects/connect_to_backend/projects/connect_to_backend/show-data-according-to-convensions/show-data-according-to-convensions.component';
-import { ShowDataBestPracticesComponent } from './projects/connect_to_backend/projects/connect_to_backend/show-data-best-practices/show-data-best-practices.component';
-import { ShowDataWithRxjsComponent } from './projects/connect_to_backend/projects/connect_to_backend/show-data-with-rxjs/show-data-with-rxjs.component';
-import { ShowDataComponent } from './projects/connect_to_backend/projects/connect_to_backend/show-data/show-data.component';
+import { ShowDataAccordingToConvensionsComponent } from './projects/connect_to_backend/show-data-according-to-convensions/show-data-according-to-convensions.component';
+import { ShowDataBestPracticesComponent } from './projects/connect_to_backend/show-data-best-practices/show-data-best-practices.component';
+import { ShowDataWithRxjsComponent } from './projects/connect_to_backend/show-data-with-rxjs/show-data-with-rxjs.component';
+import { ShowDataComponent } from './projects/connect_to_backend/show-data/show-data.component';
 
 @NgModule({
   declarations: [
