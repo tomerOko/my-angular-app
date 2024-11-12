87b154405fc60c29a090cae91cc43138c5d84deb
tomerOko <tomeroko2@gmail.com>
2024-11-12 12:38:37 +0200

    show data

--- src/app/projects/connect_to_backend/show-data/show-data.component.ts
+++ src/app/projects/connect_to_backend/show-data/show-data.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../../shared/services/api.service';
 
 @Component({
   selector: 'app-show-data',
   templateUrl: './show-data.component.html',
-  styleUrl: './show-data.component.scss'
+  styleUrl: './show-data.component.scss',
 })
 export class ShowDataComponent {
+  constructor(private http: ApiService) {}
 
+  public buttons = [
+    { text: 'test', onClick: () => this.handleClick('test') },
+    {
+      text: 'elastic post',
+      onClick: () => this.handleClick('elastic', {}),
+    },
+    {
+      text: 'elastic get',
+      onClick: () => this.handleClick('elastic'),
+    },
+    {
+      text: 'mongo post',
+      onClick: () => this.handleClick('mongo'),
+    },
+    { text: 'mongo get', onClick: () => this.handleClick('mongo') },
+  ];
+
+  public result: string = '';
+
+  async handleClick(url: string, body?: Record<string, any>): Promise<void> {
+    console.log('Sending request to backend');
+    const mehtod = body ? 'post' : 'get';
+    await this.http[mehtod](url, body).subscribe({
+      next: (response) => {
+        console.log('Response from backend:', response);
+        this.result = JSON.stringify(response);
+      },
+      error: (error) => {
+        console.error('Error from backend:', error);
+      },
+    });
+  }
 }
--- src/app/projects/connect_to_backend/show-data/show-data.module.ts
+++ src/app/projects/connect_to_backend/show-data/show-data.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowDataComponent } from './show-data.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ShowDataComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: '', component: ShowDataComponent }]),
+  ],
 })
 export class ShowDataModule {}
